78be63bb1b70e4c38984b5565abb2a8ca0339e86
Rafael <rafa_soyyo@hotmail.com>
2026-01-11 00:47:43 +0100

    calendar events

--- src/app/private/calendar/calendar.ts
+++ src/app/private/calendar/calendar.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal, ViewChild } from '@angular/core';
+import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { AgendaService } from '../../domain/agenda/agenda.service';
+import { User } from '../../domain/user/user';
+import { UserService } from '../../domain/user/user.service';
 
 @Component({
   selector: 'app-calendar',
-  imports: [FullCalendarModule],
+  standalone: true,
+  imports: [CommonModule, FullCalendarModule, DialogModule, ButtonModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
 export class Calendar implements OnInit {
+  @ViewChild('calendar') calendarComponent!: FullCalendarComponent;
+
+  visibleDialog = signal(false);
+  selectedClasses = signal<any[]>([]);
+  selectedDate = signal<string>('');
+  users = signal<User[]>([]);
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
@@ -19,33 +32,75 @@ export class Calendar implements OnInit {
     firstDay: 1,
     weekends: false,
     plugins: [dayGridPlugin, interactionPlugin],
-    dateClick: (arg) => this.handleDateClick(arg),
+    locale: 'es',
+    buttonText: {
+      today: 'Hoy',
+      month: 'Mes',
+      week: 'Semana',
+      day: 'Día',
+      list: 'Agenda'
+    },
+    eventClick: (info) => this.handleEventClick(info),
+    eventSources: [this.loadAgendaEvents.bind(this)],
+    eventTimeFormat: {
+      hour: '2-digit',
+      minute: '2-digit',
+      meridiem: false,
+      hour12: false
+    }
   };
 
+  constructor(
+    private agendaService: AgendaService,
+    private userService: UserService
+  ) { }
+
   ngOnInit(): void {
-    this.getEvents();
+    this.userService.list().then(users => this.users.set(users));
   }
 
-  getEvents(): void {
-    const days = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes'];
-    const hours = [['18:00:00', '19:15:00'], ['19:15:00', '20:30:00'], ['20:30:00', '21:45:00']];
-    const events = days
-      .flatMap((day, dayIndex) => {
-        return hours.map((hour) => {
-          return {
-            title: day,
-            daysOfWeek: [dayIndex + 1],
-            startTime: hour[0],
-            endTime: hour[1],
-            startRecur: '2026-01-01',
-            endRecur: '2026-07-31',
-          };
-        });
+  getMemberNames(memberIds: Set<string> | string[]): string[] {
+    const ids = Array.from(memberIds);
+    return this.users()
+      .filter(u => ids.includes(u.id))
+      .map(u => u.name);
+  }
+
+  loadAgendaEvents(info: any, successCallback: any, failureCallback: any): void {
+    const dayMap = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'];
+
+    this.agendaService.list()
+      .then(agendaItems => {
+        const events = agendaItems.map(item => ({
+          title: 'Clase',
+          daysOfWeek: [dayMap.indexOf(item.day) + 1],
+          startTime: item.startHour,
+          endTime: item.endHour,
+          startRecur: item.startDay,
+          endRecur: item.endDay,
+          color: '#6366f1',
+          extendedProps: {
+            agenda: item
+          }
+        }));
+        successCallback(events);
+      })
+      .catch(error => {
+        console.error('Error loading agenda events:', error);
+        failureCallback(error);
       });
-    this.calendarOptions.events = events;
   }
 
-  handleDateClick(arg: any) {
-    alert('date click! ' + arg.dateStr)
+  handleEventClick(info: any) {
+    const agenda = info.event.extendedProps.agenda;
+    const eventDate = info.event.start;
+
+    this.selectedDate.set(eventDate.toLocaleDateString('es-ES', {
+      weekday: 'long',
+      day: 'numeric',
+      month: 'long'
+    }));
+    this.selectedClasses.set([agenda]);
+    this.visibleDialog.set(true);
   }
 }
