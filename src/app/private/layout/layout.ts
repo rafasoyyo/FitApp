5ab08cf4bcad1ac11d7ed1580b6150dddee81ced
Rafael Hernández Navarro <rafael.hernandez@fourvenues.com>
2026-01-12 21:52:33 +0100

    admin tab limited

--- src/app/private/layout/layout.ts
+++ src/app/private/layout/layout.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 import { DialogModule } from 'primeng/dialog';
@@ -9,21 +10,26 @@ import { UserService } from '../../domain/user/user.service';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, TabsModule, CommonModule, DialogModule],
+  imports: [ RouterOutlet, RouterLink, TabsModule, CommonModule, DialogModule ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class Layout implements OnInit {
+  private userService = inject(UserService);
   showAbout = signal(false);
   version = signal('1.0.0');
+  user = toSignal(this.userService.currentUser$);
 
-  constructor (public router: Router, private userService: UserService) { }
+  constructor (public router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit (): void {
+    this.getVersion();
     this.userService.currentUser$.subscribe(user => {
       document.querySelector('html')?.classList.toggle('my-app-dark', user?.darkMode ?? false);
     });
+  }
 
+  getVersion () {
     fetch('https://fitcenter.web.app/ngsw.json')
       .then(res => res.json())
       .then(data => {
